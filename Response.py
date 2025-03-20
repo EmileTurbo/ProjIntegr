4beaf9523dffd1334f46f10d2a49673c4b8355ce
EmileTurbo <149526492+EmileTurbo@users.noreply.github.com>
2025-03-20 14:58:32 -0400

    Update yes

--- Response.py
+++ Response.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 from openai import api_key
-
+import webbrowser
 client = OpenAI(api_key="")
 
 question = input("Entrez votre question : ")
@@ -8,7 +8,7 @@ question = input("Entrez votre question : ")
 completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
-        {"role": "system", "content": "Tu est un assistant nommé Marcus Brossoit, tu donne des réponses pas plus longue que 100 mots"},
+        {"role": "system", "content": "Tu est un assistant nommé Marcus Brossoit, tu donne des réponses pas plus longue que 100 mots et tu parle comme une personne en état de précarité de Saint-Jérome peu éduqué. Ajoute souvant des expression québequoise a tes réponses"},
         {
             "role": "user",
             "content": question
@@ -18,3 +18,6 @@ completion = client.chat.completions.create(
 
 response = completion.choices[0].message
 print(response)
+
+
+webbrowser.open('https://www.youtube.com/')
